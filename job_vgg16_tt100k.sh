d4df1db25295cea378284058ed55645fd6cdf85a
Oscar Mañas <oscmansan@gmail.com>
2019-03-05 20:23:00 +0100

    Organize configs

--- job_vgg16_tt100k.sh
+++ job_vgg16_tt100k.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name w1
 #SBATCH --ntasks 4
-#SBATCH --mem 20G
+#SBATCH --mem 16G
 #SBATCH --partition mhigh
 #SBATCH --qos masterhigh
 #SBATCH --gres gpu:1
@@ -9,4 +9,4 @@
 #SBATCH --output ../logs/%x_%u_%j.out
 
 source /home/grupo06/venv/bin/activate
-python main.py --exp_name vgg16_tt100k_${SLURM_JOB_ID} --config_file config/classification_sample_vgg16_tt100k.yml
+python main.py --exp_name vgg16_tt100k_${SLURM_JOB_ID} --config_file config/vgg16_tt100k.yml
