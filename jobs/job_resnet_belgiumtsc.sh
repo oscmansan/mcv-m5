07f1dfb6d8220680e4fd90117a2f25160f333fa7
Jorge López <jorge.lopez@neosentec.com>
2019-03-06 16:24:40 +0100

    Added more jobs

---
+++ jobs/job_resnet_belgiumtsc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH --job-name w2
+#SBATCH --ntasks 4
+#SBATCH --mem 16G
+#SBATCH --partition mhigh,mlow
+#SBATCH --gres gpu:1
+#SBATCH --chdir /home/grupo06/m5-project
+#SBATCH --output ../logs/%x_%u_%j.out
+
+source /home/grupo06/venv/bin/activate
+python src/main.py --exp_name resnet_tt100k_${SLURM_JOB_ID} --config_file config/resnet_belgiumtsc.yml
