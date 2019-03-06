55d09957417a1a9257b5618c2c5c28a474ff49d0
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-03-06 11:48:02 +0100

    Added belgiumtsc and kitti jobs

---
+++ job_vgg16_kitti.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name w2
+#SBATCH --ntasks 4
+#SBATCH --mem 16G
+#SBATCH --partition mhigh
+#SBATCH --qos masterhigh
+#SBATCH --gres gpu:1
+#SBATCH --chdir /home/grupo06/m5-project
+#SBATCH --output ../logs/%x_%u_%j.out
+
+source /home/grupo06/venv/bin/activate
+python main.py --exp_name vgg16_tt100k_${SLURM_JOB_ID} --config_file config/vgg16_kitti.yml
