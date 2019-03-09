ebf98eb64ad6cebfb89d14b0bb26e7802f747ecb
pablorodriper <pablorodriper@gmail.com>
2019-03-09 03:30:50 +0100

    Fix flip transformation

--- src/utils/transformations/flip.py
+++ src/utils/transformations/flip.py
@@ -7,30 +7,33 @@ from PIL import Image
 class RandomHorizontalFlipSegSem:
     def __init__(self, cf):
         self.cf = cf
-
+    
     def __call__(self, img, gt):
         if self.cf.hflips and random.random() < 0.5:
-            return np.fliplr(img), np.fliplr(gt)
+            if gt is not None:
+                return np.fliplr(img), np.fliplr(gt)
+            else:
+                return np.fliplr(img), gt
         return img, gt
 
 
 class RandomHorizontalFlipObjDet:
     def __init__(self, cf):
         self.cf = cf
-
+    
     def __call__(self, img, boxes, labels):
         """Randomly flip PIL image.
-
-        If boxes is not None, flip boxes accordingly.
-
-        Args:
-          img: (PIL.Image) image to be flipped.
-          boxes: (tensor) object boxes, sized [#obj,4].
-
-        Returns:
-          img: (PIL.Image) randomly flipped image.
-          boxes: (tensor) randomly flipped boxes.
-        """
+            
+            If boxes is not None, flip boxes accordingly.
+            
+            Args:
+            img: (PIL.Image) image to be flipped.
+            boxes: (tensor) object boxes, sized [#obj,4].
+            
+            Returns:
+            img: (PIL.Image) randomly flipped image.
+            boxes: (tensor) randomly flipped boxes.
+            """
         if random.random() < 0.5:
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
             w = img.width
