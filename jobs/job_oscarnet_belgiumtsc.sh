0258084df7493f37defdd20a25514c1d839debe8
Oscar Mañas <oscmansan@gmail.com>
2019-03-09 18:44:22 +0100

    Add oscarnet_belgiumtsc experiment

--- jobs/job_oscarnet_belgiumtsc.sh
+++ jobs/job_oscarnet_belgiumtsc.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name oscarnet_tt100k
 #SBATCH --ntasks 4
 #SBATCH --mem 16G
-#SBATCH --partition mhigh,mlow
+#SBATCH --priority mhigh
+#SBATCH --partition mhigh
 #SBATCH --gres gpu:1
 #SBATCH --chdir /home/grupo06/.oscar/m5-project
 #SBATCH --output ../logs/%x_%u_%j.out
 
 source /home/grupo06/venv/bin/activate
-python src/main.py --exp_name oscarnet_tt100k_${SLURM_JOB_ID} --config_file config/oscarnet_tt100k.yml
+python src/main.py --exp_name oscarnet_belgiumtsc_${SLURM_JOB_ID} --config_file config/oscarnet_belgiumtsc.yml
