9ab6d650d591362f698bea373181dba27422892e
Oscar Mañas <oscmansan@gmail.com>
2019-03-09 19:34:21 +0100

    Add oscarnet_kitti experiment

--- jobs/job_oscarnet_kitti.sh
+++ jobs/job_oscarnet_kitti.sh
@@ -3,10 +3,10 @@
 #SBATCH --ntasks 4
 #SBATCH --mem 16G
 #SBATCH --priority mhigh
-#SBATCH --partition mhigh
+#SBATCH --partition mhigh,mlow
 #SBATCH --gres gpu:1
 #SBATCH --chdir /home/grupo06/.oscar/m5-project
 #SBATCH --output ../logs/%x_%u_%j.out
 
 source /home/grupo06/venv/bin/activate
-python src/main.py --exp_name oscarnet_tt100k_${SLURM_JOB_ID} --config_file config/oscarnet_tt100k.yml
+python src/main.py --exp_name oscarnet_kitti_${SLURM_JOB_ID} --config_file config/oscarnet_kitti.yml
