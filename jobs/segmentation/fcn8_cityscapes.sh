e8f40f6f9d5b50f7a858a72272b48176a02723b2
Oscar Mañas <oscmansan@gmail.com>
2019-03-16 17:45:47 +0100

    Refactor configs and jobs

---
+++ jobs/segmentation/fcn8_cityscapes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name fcn8_cityscapes
+#SBATCH --ntasks 4
+#SBATCH --mem 16G
+#SBATCH --qos masterhigh
+#SBATCH --partition mhigh,mlow
+#SBATCH --gres gpu:1
+#SBATCH --chdir /home/grupo06/m5-project
+#SBATCH --output ../logs/%x_%j.out
+
+source /home/grupo06/venv/bin/activate
+python src/main.py --exp_name fcn8_cityscapes_${SLURM_JOB_ID} --config_file config/segmentation/fcn8_cityscapes.yml
